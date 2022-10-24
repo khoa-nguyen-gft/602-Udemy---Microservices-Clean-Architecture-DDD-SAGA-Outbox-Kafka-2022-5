98372eb43f31d16158f9f963f37d9928a4d89c1e
Nguyen, Khoa <npdangkhoa@gmail.com>
2022-10-24 09:57:15 +0700

    13. Implementing Order Application Service - DTO classes.mp4

---
+++ food-ordering-system/order-service/order-domains/order-domain-service/src/main/java/com/food/ordering/system/order/service/domain/dto/dto/OrderAddress.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain.dto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author kany
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderAddress {
+    @NotNull
+    @Max(value = 50)
+    private final String street;
+    @NotNull
+    @Max(value = 10)
+    private final String postalCode;
+    @NotNull
+    @Max(value = 50)
+    private final String city;
+}
---
+++ food-ordering-system/order-service/order-domains/order-domain-service/src/main/java/com/food/ordering/system/order/service/domain/dto/track/TrackOrderQuery.java
@@ -0,0 +1,20 @@
+package com.food.ordering.system.order.service.domain.dto.track;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+/**
+ * @author kany
+ */
+@Getter
+@Builder
+@AllArgsConstructor
+public class TrackOrderQuery {
+
+    @NotNull
+    private final UUID orderTrackingId;
+}
